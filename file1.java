f9744db1f2a5c0caaa56da6f3516da84929916cc
EC2 Default User <ec2-user@ip-172-31-22-188.ap-south-1.compute.internal>
2019-05-09 20:29:45 +0000

    my first commit

---
+++ file1.java
@@ -0,0 +1,8 @@
+public class file1
+          {
+                 public static void main(String[] args)
+                 {
+                        System.out.println("jenkins integration with git");
+                    }
+
+               }
